3ec9bd812d478aec191c2620525d1d264cdbfaab
rauniyarrishav951 <rauniyarrishav951@gmail.com>
2024-02-29 12:44:28 +0545

    Test.java

---
+++ Week1/Test.java
@@ -0,0 +1,12 @@
+// 2. “Test”. Create a project, enter, compile and run the following application.
+
+public class Test
+{
+   //----------------------------------------------------
+   //  Prints a statement.
+   //----------------------------------------------------
+   public static void main (String[] args)
+   {
+      System.out.println ("An Emergency Broadcast");
+   }
+}
