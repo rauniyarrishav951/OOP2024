265986d79fcc6ce1d33e331d1402a6b17b9fe892
rauniyarrishav951 <rauniyarrishav951@gmail.com>
2024-02-29 12:51:10 +0545

    Diamond.java

---
+++ Week1/Diamond.java
@@ -0,0 +1,21 @@
+// 4. Write and application that prints the following diamond shape. Don’t print any unneeded characters
+public class Diamond
+{
+    public static void main(String args[])
+    {
+        int a=4;
+
+        for(int i=0; i<a;i++){
+
+            for(int j=0; j<a-i-1;j++)
+        
+        {
+            System.out.println(" ");
+        }
+        for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
