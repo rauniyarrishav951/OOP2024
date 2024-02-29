b338f6a2de48ca80d762414fe0d35af5203bfbec
rauniyarrishav951 <rauniyarrishav951@gmail.com>
2024-02-29 12:48:33 +0545

    PersonalDetails.java

---
+++ Week1/PersonalDetails.java
@@ -0,0 +1,22 @@
+// 3. Create a second project called “PersonalDetails” and write a program that outputs some personal details to the screen, e.g. Name, Address, Age, Phone number etc.
+
+public class PersonalDetails
+{
+    public static void main(String args[])
+    {
+        
+        String Name = "Rishav"; 
+        int age = 20;
+        int Phone = 983749484;
+        String address = "abcd";
+        
+        System.out.println(Name);
+        System.out.println(age);
+        System.out.println(Phone);
+        System.out.println(address);
+
+
+
+
+    }
+}
