384e0158c1b57e0333f4dc2c215e094c68b39fcd
rauniyarrishav951 <rauniyarrishav951@gmail.com>
2024-03-11 18:29:42 +0545

    Rock.java

---
+++ Week3/Rock.java
@@ -0,0 +1,63 @@
+// 2) Rock, Paper, Scissors
+
+// Program Rock.java contains a skeleton for the game Rock, Paper, Scissors. Add statements to the program as indicated by the comments so that the program asks the user to enter a play, generates a random play for the computer, compares them and announces the winner (and why).
+// Note that the user should be able to enter either upper or lower case r, p, and s. The user's play is stored as a string to make it easy to convert whatever is entered to upper case. Use a switch statement to convert the randomly generated integer for the computer's play to a string.
+
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class Rock
+{
+   public static void main(String[] args)
+   {
+        String personPlay;    //User's play -- "R", "P", or "S"
+        String computerPlay;  //Computer's play -- "R", "P", or "S"
+        int computerInt;      //Randomly generated number used to determine
+                              //computer's play
+
+
+        Scanner scan = new Scanner(System.in);
+        Random generator = new Random();
+
+
+        //Get player's play -- note that this is stored as a string
+        //Make player's play uppercase for ease of comparison
+        System.out.println("Enter player's play:");
+        personPlay = scan.next().toUpperCase();
+       
+        //Generate computer's play (0,1,2)
+        computerInt=generator.nextInt(3);
+
+        //Translate computer's randomly generated play to string
+        computerPlay = switch(computerInt) {
+        case 0 -> "R";
+        case 1 -> "P";
+        case 2 -> "S";
+        default -> "Error";
+};
+
+
+
+        //Print computer's play
+        System.out.println("Computer's play: " + computerPlay);
+
+        //See who won.  Use nested ifs instead of &&.
+        if (personPlay.equals(computerPlay))  
+            System.out.println("It's a tie!");
+        else if (personPlay.equals("R"))
+            if (computerPlay.equals("S"))
+                System.out.println("Rock crushes scissors.  You win!!");
+            else
+            System.out.println("Paper covers rock. You lose!!");
+        else if (personPlay.equals("P"))
+            if (computerPlay.equals("R"))
+                System.out.println("Paper covers rock. You win!!");
+            else
+            
+            System.out.println("Invalid input. Please enter R, P, or S.");
+
+
+                //...  Fill in rest of code
+   }
+}
